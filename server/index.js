fe2928c93ae5361fc5835cfd8f8a6f1bf2c45635
lichessapps <lichessapps@gmail.com>
2018-05-31 20:54:10 +0200

    Apps

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 "use strict";
 var schedule = require('node-schedule');
 console.log("scheduler startup");
-schedule.scheduleJob('0-59 * * * *', function () {
-    console.log("tick");
-});
+let HAPPS = process.env.HAPPS || "";
+let apps = HAPPS.split(";");
+for (let app of apps) {
+    let parts = app.split(":");
+    let token = parts[0];
+    let name = parts[1];
+    console.log("token", token, "name", name);
+}
+/*schedule.scheduleJob('0-59 * * * *', function(){
+    console.log("tick")
+})*/
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
--- server/scheduler.ts
+++ server/scheduler.ts
@@ -1,7 +1,18 @@
 var schedule = require('node-schedule')
 
 console.log("scheduler startup")
+
+let HAPPS = process.env.HAPPS || ""
+
+let apps = HAPPS.split(";")
+
+for(let app of apps){
+    let parts = app.split(":")
+    let token = parts[0]
+    let name = parts[1]
+    console.log("token",token,"name",name)
+}
  
-schedule.scheduleJob('0-59 * * * *', function(){
+/*schedule.scheduleJob('0-59 * * * *', function(){
     console.log("tick")
-})
+})*/
