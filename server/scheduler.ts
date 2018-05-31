f09e195acdf685892c397aec482195ca59e451c6
lichessapps <lichessapps@gmail.com>
2018-05-31 21:05:01 +0200

    Set quantity

--- server/scheduler.ts
+++ server/scheduler.ts
@@ -1,18 +1,46 @@
 var schedule = require('node-schedule')
 
+const Heroku = require('heroku-client')
+
 console.log("scheduler startup")
 
 let HAPPS = process.env.HAPPS || ""
 
 let apps = HAPPS.split(";")
 
-for(let app of apps){
-    let parts = app.split(":")
-    let token = parts[0]
-    let name = parts[1]
-    console.log("token",token,"name",name)
+function setquantity(quantity:number = 1){
+    let i=0
+    for(let app of apps){
+        let parts = app.split(":")
+
+        let token = parts[0]
+        let name = parts[1]
+
+        console.log("token",token,"name",name)
+
+        i++
+
+        setTimeout(function(){
+            console.log("\ncreating client", token)
+
+            let client = new Heroku({ token: token })            
+
+            let url = '/apps/'+name+"/formation"
+
+            console.log("\ncreating request", url)
+
+            client.patch(url, {body:{"updates":[{"type":"worker", "quantity": quantity}]}}).
+                then((apps:any) => {
+                    console.log("\nresponse for", token, name, "\n")
+                    console.log(apps)
+                })
+
+        }, i*10000)
+    }
 }
- 
+
+setquantity(1)
+
 /*schedule.scheduleJob('0-59 * * * *', function(){
     console.log("tick")
 })*/
