e221b102a29a59b948d57e7913f656fa60fc86ab
lichessapps <lichessapps@gmail.com>
2018-08-21 08:28:54 +0200

    Fewer restarts

--- server/index.js
+++ server/index.js
@@ -100,28 +100,26 @@ function schedulerestart(chron, func, from, to) {
     });
 }
 const RESTARTS = [
-    ['0 8 * * *', setquantityw, 0, 1],
-    ['0 9 * * *', setquantityw, 0, 1],
-    ['0 10 * * *', setquantityw, 0, 1],
+    //
+    //['0 8 * * *',  setquantityw, 0, 1],
+    //['0 9 * * *',  setquantityw, 0, 1],
+    //['0 10 * * *', setquantityw, 0, 1],
     ['0 11 * * *', setquantityw, 0, 1],
-    ['0 12 * * *', setquantityw, 0, 1],
-    ['0 13 * * *', setquantityw, 0, 1],
-    ['0 15 * * *', setquantity, 0, 1],
-    ['0 16 * * *', setquantity, 0, 1],
-    ['0 17 * * *', setquantity, 0, 1],
+    //['0 12 * * *', setquantityw, 0, 1],
+    //['0 13 * * *', setquantityw, 0, 1],
+    //
+    //['0 15 * * *', setquantity,  0, 1],
+    //['0 16 * * *', setquantity,  0, 1],
+    //['0 17 * * *', setquantity,  0, 1],
     ['0 18 * * *', setquantity, 0, 1],
-    ['0 19 * * *', setquantity, 0, 1],
-    ['0 20 * * *', setquantity, 0, 1],
-    ['0 21 * * *', setquantity, 0, 1],
+    //['0 19 * * *', setquantity,  0, 1],
+    //['0 20 * * *', setquantity,  0, 1],
+    //['0 21 * * *', setquantity,  0, 1],
     ['0 22 * * *', setquantity, 0, 1],
-    ['0 23 * * *', setquantity, 0, 1],
-    ['0 0 * * *', setquantity, 0, 1],
-    ['0 1 * * *', setquantity, 0, 1],
+    //['0 23 * * *', setquantity,  0, 1],
+    //['0 0 * * *',  setquantity,  0, 1],
+    //['0 1 * * *',  setquantity,  0, 1],
     ['0 2 * * *', setquantity, 0, 1],
-    ['0 3 * * *', setquantity, 0, 1],
-    ['0 4 * * *', setquantity, 0, 1],
-    ['0 5 * * *', setquantity, 0, 1],
-    ['0 6 * * *', setquantity, 0, 1]
 ];
 for (let rs of RESTARTS) {
     schedulerestart(rs[0], rs[1], rs[2], rs[3]);
--- server/restart.ts
+++ server/restart.ts
@@ -19,28 +19,30 @@ function schedulerestart(chron: string, func:any, from:number, to:number){
 }
 
 const RESTARTS:[string, any, number, number][] = [
-    ['0 8 * * *',  setquantityw, 0, 1],
-    ['0 9 * * *',  setquantityw, 0, 1],
-    ['0 10 * * *', setquantityw, 0, 1],
+    //
+    //['0 8 * * *',  setquantityw, 0, 1],
+    //['0 9 * * *',  setquantityw, 0, 1],
+    //['0 10 * * *', setquantityw, 0, 1],
     ['0 11 * * *', setquantityw, 0, 1],
-    ['0 12 * * *', setquantityw, 0, 1],
-    ['0 13 * * *', setquantityw, 0, 1],
-    ['0 15 * * *', setquantity,  0, 1],
-    ['0 16 * * *', setquantity,  0, 1],
-    ['0 17 * * *', setquantity,  0, 1],
+    //['0 12 * * *', setquantityw, 0, 1],
+    //['0 13 * * *', setquantityw, 0, 1],
+    //
+    //['0 15 * * *', setquantity,  0, 1],
+    //['0 16 * * *', setquantity,  0, 1],
+    //['0 17 * * *', setquantity,  0, 1],
     ['0 18 * * *', setquantity,  0, 1],
-    ['0 19 * * *', setquantity,  0, 1],
-    ['0 20 * * *', setquantity,  0, 1],
-    ['0 21 * * *', setquantity,  0, 1],
+    //['0 19 * * *', setquantity,  0, 1],
+    //['0 20 * * *', setquantity,  0, 1],
+    //['0 21 * * *', setquantity,  0, 1],
     ['0 22 * * *', setquantity,  0, 1],
-    ['0 23 * * *', setquantity,  0, 1],
-    ['0 0 * * *',  setquantity,  0, 1],
-    ['0 1 * * *',  setquantity,  0, 1],
+    //['0 23 * * *', setquantity,  0, 1],
+    //['0 0 * * *',  setquantity,  0, 1],
+    //['0 1 * * *',  setquantity,  0, 1],
     ['0 2 * * *',  setquantity,  0, 1],
-    ['0 3 * * *',  setquantity,  0, 1],
-    ['0 4 * * *',  setquantity,  0, 1],
-    ['0 5 * * *',  setquantity,  0, 1],
-    ['0 6 * * *',  setquantity,  0, 1]
+    //['0 3 * * *',  setquantity,  0, 1],
+    //['0 4 * * *',  setquantity,  0, 1],
+    //['0 5 * * *',  setquantity,  0, 1],
+    //['0 6 * * *',  setquantity,  0, 1]
 ]
 
 for(let rs of RESTARTS){
